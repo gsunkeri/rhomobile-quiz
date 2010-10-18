729c1c803d7e9e14334a80b4ab36dee7558a4d9a
eivindgreibrokk <=>
2010-10-18 15:25:12 +0200

    fixing location

--- app/Question/controller.rb
+++ app/Question/controller.rb
@@ -21,7 +21,22 @@ class QuestionController < Rho::RhoController
       app_info(@question)
       if @question.path == 'rotation' && @question.answer != "true"
         System::set_screen_rotation_notification(url_for(:action => :screenRotateCallback, :query => {:id => @question.object}), "")
+      elsif @question.path == "location" && @question.long != nil && @question.answer != "true"
+        app_info("calculating distance...")
+        app_info(GeoLocation.latitude.to_s)
+        app_info(GeoLocation.longitude.to_s)
+        app_info(@question.lat)
+        app_info(@question.long)
+        
+        @distance = ( GeoLocation.haversine_distance @question.lat.to_f, @question.long.to_f, GeoLocation.latitude, GeoLocation.longitude ) * 1609
+     if (@distance > 100)
+          @question.update_attributes({"answer" => "true"})
+        end
       end
+
+      app_info(@distance)
+      app_info(@question)
+      
       render :action => 'show_' + @question.path
     else
       redirect :action => :index
@@ -34,7 +49,13 @@ class QuestionController < Rho::RhoController
 
     #can we stop it from rendering anything here? We just want it to stay on the same page
     @question = Question.find(@params['id'])
-    render :action => 'show_sound'  
+    render :action => 'show_sound'
+  end
+
+  def tracking
+    @question = Question.find(@params['id'])
+    @question.update_attributes({"long" => GeoLocation.longitude, "lat" => GeoLocation.latitude})
+    render :action => 'show_location'
   end
 
   def update
