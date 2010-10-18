7dd817c7ed705df7a935a5e73485fbb2989e3d32
Eivind Greibrokk <eivind@Eivinds-MBP.local>
2010-10-18 11:14:44 +0200

    fixed accelerometer so that it works in iPhone Simulator

--- app/Question/controller.rb
+++ app/Question/controller.rb
@@ -7,6 +7,9 @@ class QuestionController < Rho::RhoController
   #GET /Question
   def index
     @questions = Question.find(:all)
+    app_info(@questions)
+    System::set_screen_rotation_notification(url_for(:action => :emptyScreenRotateCallback), "")
+    
     render
   end
 
@@ -14,15 +17,16 @@ class QuestionController < Rho::RhoController
   def show
     @question = Question.find(@params['id'])
     if @question
-	#		if @question.path == 'accelerometer'
-#				System::set_screen_rotation_notification(url_for(:action => :screenRotateCallback, :query => {:id => @question.object}), "" )
-	#		end
+      app_info(@question)
+      if @question.path == 'accelerometer' && @question.answer != "true"
+        System::set_screen_rotation_notification(url_for(:action => :screenRotateCallback, :query => {:id => @question.object}), "")
+      end
       render :action => 'show_' + @question.path
     else
       redirect :action => :index
     end
   end
-  
+
   def play_sound
     Alert.play_file '/public/seagull.mp3', 'audio/mpeg'
     render :action => 'show_sound'
@@ -35,10 +39,22 @@ class QuestionController < Rho::RhoController
   end
 
   def screenRotateCallback
-		@question = Question.find(@params['id'])
-		@question.answer = @params['degrees']
-		@question.save
-		redirect :action => 'show_accelerometer_answered'
+    @question = Question.find(@params["id"])
+    @question.update_attributes({"answer" => "true"})
+    app_info(@question)
+
+    Alert.show_popup :title => "Success", :message => "Screen rotated successfully!", :icon => :info,
+                     :buttons => ["OK"],
+                     :callback => url_for(:action => :on_popup_close)
   end
- 
+
+  def emptyScreenRotateCallback
+
+  
+  end
+
+  def on_popup_close
+    WebView.navigate(url_for(:action => :index))
+  end
+
 end
