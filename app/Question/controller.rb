554c4cc15afb41bb0f268896d9d5dbc3e4061e4b
Bjarte Stien Karlsen <bjarte@bjartek.org>
2010-10-21 12:16:35 +0200

    trying to fix back button issue when playing sound and reindent som code

--- app/Question/controller.rb
+++ app/Question/controller.rb
@@ -9,7 +9,6 @@ class QuestionController < Rho::RhoController
     @questions = Question.find(:all)
     #how can we unregister the previous callback? Hacking here, just setting a empty callback
     System::set_screen_rotation_notification(url_for(:action => :emptyScreenRotateCallback), "")
-
     render
   end
 
@@ -21,7 +20,7 @@ class QuestionController < Rho::RhoController
       if @question.path == 'rotation' && @question.answer != "true"
         System::set_screen_rotation_notification(url_for(:action => :screenRotateCallback, :query => {:id => @question.object}), "")
       elsif @question.path == "location" && @question.long != nil && @question.answer != "true"
-        @distance = ( GeoLocation.haversine_distance @question.lat.to_f, @question.long.to_f, GeoLocation.latitude, GeoLocation.longitude ) * 1609
+        @distance = (GeoLocation.haversine_distance @question.lat.to_f, @question.long.to_f, GeoLocation.latitude, GeoLocation.longitude) * 1609
         if (@distance > 100)
           @question.update_attributes({"answer" => "true"})
         end
@@ -38,7 +37,7 @@ class QuestionController < Rho::RhoController
     Alert.play_file '/public/seagull.mp3', 'audio/mpeg'
     #can we stop it from rendering anything here? We just want it to stay on the same page
     @question = Question.find(@params['id'])
-    render :action => 'show_sound'
+    render :action => 'show_sound', :back => "/app/Question"
   end
 
   def tracking
@@ -51,19 +50,21 @@ class QuestionController < Rho::RhoController
     @question = Question.find(@params['id'])
     @question.update_attributes(@params['question']) if @question
     redirect :action => :index
+
   end
 
   def screenRotateCallback
     @question = Question.find(@params["id"])
     @question.update_attributes({"answer" => "true"})
 
-    Alert.show_popup :title => "Success", :message => "Screen rotated successfully!", :icon => :info,
-      :buttons => ["OK"],
-      :callback => url_for(:action => :on_popup_close)
+    Alert.show_popup :title => "Success",
+                     :message => "Screen rotated successfully!",
+                     :icon => :info,
+                     :buttons => ["OK"],
+                     :callback => url_for(:action => :on_popup_close)
   end
 
   def emptyScreenRotateCallback
-
 
   end
 
@@ -78,14 +79,14 @@ class QuestionController < Rho::RhoController
   end
 
   def camera_callback
-		@question = Question.find(@params['id'])
-		@code = Barcode.barcode_recognize(Rho::RhoApplication::get_blob_path(@params['image_uri']))
-		if @code != ""
-			@question.update_attributes({"answer" => @code})
-		end
-		app_info("Barcode")
-		app_info(@code)
-    WebView.navigate( url_for :action => :show, :id => @params['id'], :query => { :try => "true" })
+    @question = Question.find(@params['id'])
+    @code = Barcode.barcode_recognize(Rho::RhoApplication::get_blob_path(@params['image_uri']))
+    if @code != ""
+      @question.update_attributes({"answer" => @code})
+    end
+    app_info("Barcode")
+    app_info(@code)
+    WebView.navigate(url_for :action => :show, :id => @params['id'], :query => {:try => "true"})
   end
 
   def submit
